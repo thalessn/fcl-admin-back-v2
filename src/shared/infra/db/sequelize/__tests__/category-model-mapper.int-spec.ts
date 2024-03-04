035a2bab518437ec3af5e618227e6f7a356b2a30
Thales Nascimento <thales.silva.nascimento@gmail.com>
2024-03-04 18:38:43 -0300

    test: category model mapper

---
+++ src/shared/infra/db/sequelize/__tests__/category-model-mapper.int-spec.ts
@@ -0,0 +1,68 @@
+import { Sequelize } from "sequelize-typescript";
+import { CategoryModel } from "../category.model";
+import { CategoryModelMapper } from "../category-model-mapper";
+import { EntityValidationError } from "../../../../domain/validators/validation.error";
+import { Uuid } from "../../../../domain/value-objects/uuid.vo";
+import { Category } from "../../../../../category/domain/category.entity";
+
+describe("CategoryModelMapper Integration Test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      models: [CategoryModel],
+      logging: false,
+    });
+    await sequelize.sync({ force: true });
+  });
+
+  it("should throw error when category is invalid", () => {
+    const model = CategoryModel.build({
+      category_id: "3c2964e1-c6d8-4d9d-bb68-1f83a0d2f388",
+    });
+
+    try {
+      CategoryModelMapper.toEntity(model);
+      fail("The category is valid, but it needs throw a EntityValidationError");
+    } catch (e) {
+      expect(e).toBeInstanceOf(EntityValidationError);
+      expect((e as EntityValidationError).error).toMatchObject({
+        name: [
+          "name should not be empty",
+          "name must be a string",
+          "name must be shorter than or equal to 255 characters",
+        ],
+      });
+    }
+  });
+
+  it("should convert a category model to a category entity", () => {
+    const created_at = new Date();
+    const model = CategoryModel.build({
+      category_id: "3c2964e1-c6d8-4d9d-bb68-1f83a0d2f388",
+      name: "Test",
+      description: "some description",
+      is_active: true,
+      created_at,
+    });
+
+    const category = CategoryModelMapper.toEntity(model);
+    expect(category.toJSON()).toStrictEqual({
+      category_id: "3c2964e1-c6d8-4d9d-bb68-1f83a0d2f388",
+      name: model.name,
+      description: model.description,
+      is_active: model.is_active,
+      created_at,
+    });
+  });
+
+  it("should covert a category entity to a category model", () => {
+    const category = Category.fake().aCategory().build();
+
+    const categoryModel = CategoryModelMapper.toModel(category);
+
+    expect(categoryModel.toJSON()).toMatchObject(category.toJSON());
+  });
+});
--- src/shared/infra/db/sequelize/__tests__/category.model.int-spec.ts
+++ src/shared/infra/db/sequelize/__tests__/category.model.int-spec.ts
@@ -1,17 +1,21 @@
-import { Sequelize } from "sequelize-typescript";
+import { DataType, Sequelize } from "sequelize-typescript";
 import { CategoryModel } from "../category.model";
 import { Category } from "../../../../../category/domain/category.entity";
 
 describe("CategoryModel Integration Tests", () => {
-  test("should create a category", async () => {
-    const sequelize = new Sequelize({
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
       dialect: "sqlite",
       storage: ":memory:",
       models: [CategoryModel],
+      logging: false,
     });
-
     await sequelize.sync({ force: true });
+  });
 
+  test("should create a category", async () => {
     const category = Category.fake().aCategory().build();
 
     await CategoryModel.create({
@@ -21,5 +25,45 @@ describe("CategoryModel Integration Tests", () => {
       is_active: category.is_active,
       created_at: category.created_at,
     });
+  });
+
+  test("mapping props", () => {
+    const attributeMap = CategoryModel.getAttributes();
+    const attributes = Object.keys(CategoryModel.getAttributes());
+    expect(attributes).toStrictEqual([
+      "category_id",
+      "name",
+      "description",
+      "is_active",
+      "created_at",
+    ]);
+
+    const categoryIdAttr = attributeMap.category_id;
+    expect(categoryIdAttr).toMatchObject({
+      field: "category_id",
+      primaryKey: true,
+      type: DataType.UUID(),
+    });
+
+    const nameAttr = attributeMap.name;
+    expect(nameAttr).toMatchObject({
+      field: "name",
+      fieldName: "name",
+      allowNull: false,
+      type: DataType.STRING(255),
+    });
+  });
+
+  test("create", async () => {
+    const arrange = {
+      category_id: "30b75323-0b8f-4ead-87a4-b045ba0a0678",
+      name: "Test",
+      is_active: true,
+      created_at: new Date(),
+    };
+
+    const category = await CategoryModel.create(arrange);
+
+    expect(category.toJSON()).toStrictEqual(arrange);
   });
 });
