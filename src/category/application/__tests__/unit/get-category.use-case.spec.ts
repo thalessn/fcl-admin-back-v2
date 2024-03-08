27f2cbe365b0527d3d2cd62274145725114db1fe
Thales Nascimento <thales.silva.nascimento@gmail.com>
2024-03-08 11:59:55 -0300

    test: add use cases tests

---
+++ src/category/application/__tests__/unit/get-category.use-case.spec.ts
@@ -0,0 +1,45 @@
+import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
+import {
+  InvalidUuidError,
+  Uuid,
+} from "../../../../shared/domain/value-objects/uuid.vo";
+import { Category } from "../../../domain/category.entity";
+import { CategoryInMemoryRepository } from "../../../infra/category-in-memory.repository";
+import { GetCategoryUsecase } from "../../get-category.use-case";
+
+describe("Get Category Use Case Unit Test", () => {
+  let repository: CategoryInMemoryRepository;
+  let usecase: GetCategoryUsecase;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    usecase = new GetCategoryUsecase(repository);
+  });
+
+  it("should throw an error when entity not found", async () => {
+    await expect(usecase.execute({ id: "fake_id" })).rejects.toThrow(
+      new InvalidUuidError()
+    );
+
+    const uuid = new Uuid();
+    await expect(
+      usecase.execute({
+        id: uuid.id,
+      })
+    ).rejects.toThrow(new NotFoundError(uuid.id, Category));
+  });
+
+  it("should get a category", async () => {
+    const category = Category.fake().aCategory().build();
+    repository.items = [category];
+
+    const categoryFound = await repository.findById(category.category_id);
+    expect(categoryFound).toStrictEqual({
+      id: repository.items[0].category_id,
+      name: repository.items[0].name,
+      description: repository.items[0].description,
+      is_active: repository.items[0].is_active,
+      created_at: repository.items[0].created_at,
+    });
+  });
+});
