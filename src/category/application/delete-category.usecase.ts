c61d70b2aa904c8e0dd582760c36dd909123c6ee
Thales Nascimento <thales.silva.nascimento@gmail.com>
2024-03-08 11:57:39 -0300

    feat: add delete category usecase

---
+++ src/category/application/delete-category.usecase.ts
@@ -0,0 +1,20 @@
+import { IUseCase } from "../../shared/application/use-case.interface";
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { ICategoryRepository } from "../domain/category.repository";
+
+export class DeleteCategoryUsecase
+  implements IUseCase<DeleteCategoryInput, DeleteCategoryOutPut>
+{
+  constructor(private categoryRepo: ICategoryRepository) {}
+
+  async execute(input: DeleteCategoryInput): Promise<void> {
+    const uuid = new Uuid(input.id);
+    await this.categoryRepo.delete(uuid);
+  }
+}
+
+type DeleteCategoryInput = {
+  id: string;
+};
+
+type DeleteCategoryOutPut = void;
