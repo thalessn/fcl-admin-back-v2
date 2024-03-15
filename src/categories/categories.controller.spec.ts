7720c9e0746f2409c2f574fd73edfaf11b6bca55
Thales Nascimento <thales.silva.nascimento@gmail.com>
2024-03-15 09:54:54 -0300

    feat: add config module

--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -2,16 +2,19 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
 import { DatabaseModule } from '../database/database.module';
 import { CategoriesModule } from './categories.module';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '@nestjs/config';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [DatabaseModule, CategoriesModule],
+      imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
+    console.log(module.get(ConfigService).get('DB_HOST'));
   });
 
   it('should be defined', () => {
