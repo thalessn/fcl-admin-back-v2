50e5ce904a6573675da6a841f3075f257dc05b99
Thales Nascimento <thales.silva.nascimento@gmail.com>
2024-03-21 22:46:29 -0300

    feat: add deleteUseCase to controller

--- src/nest_modules/categories-module/categories.controller.ts
+++ src/nest_modules/categories-module/categories.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   Inject,
   ParseUUIDPipe,
+  HttpCode,
 } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
@@ -60,8 +61,13 @@ export class CategoriesController {
     return CategoriesController.serialize(output);
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  remove(@Param('id') id: string) {}
+  remove(
+    @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
+  ) {
+    return this.deleteCategoryUseCase.execute({ id });
+  }
 
   static serialize(output: CategoryOutput) {
     return new CategoryPresenter(output);
