429a49064fa5cd8524f5f2e72dd6ed03b06fceeb
Thales Nascimento <thales.silva.nascimento@gmail.com>
2024-03-21 22:26:11 -0300

    feat: add validation on createCategory route
    Add the class validator to createCategoryDTO and change the default error code
    to 422

--- src/nest_modules/categories-module/categories.controller.ts
+++ src/nest_modules/categories-module/categories.controller.ts
@@ -15,6 +15,8 @@ import { UpdateCategoryUseCase } from 'src/core/category/application/use-cases/u
 import { ListCategoriesUseCase } from 'src/core/category/application/use-cases/list-category/list-categories.use-case';
 import { DeleteCategoryUsecase } from 'src/core/category/application/use-cases/delete-category/delete-category.usecase';
 import { GetCategoryUsecase } from 'src/core/category/application/use-cases/get-category/get-category.use-case';
+import { CategoryPresenter } from './categories.presenter';
+import { CategoryOutput } from 'src/core/category/application/use-cases/common/category-output';
 
 @Controller('categories')
 export class CategoriesController {
@@ -34,7 +36,10 @@ export class CategoriesController {
   private getCategoryUseCase: GetCategoryUsecase;
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {}
+  async create(@Body() createCategoryDto: CreateCategoryDto) {
+    const output = await this.createCategoryUseCase.execute(createCategoryDto);
+    return CategoriesController.serialize(output);
+  }
 
   @Get()
   findAll() {}
@@ -50,4 +55,8 @@ export class CategoriesController {
 
   @Delete(':id')
   remove(@Param('id') id: string) {}
+
+  static serialize(output: CategoryOutput) {
+    return new CategoryPresenter(output);
+  }
 }
