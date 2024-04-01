d4e1e8de4c5e2eb226f08243b5f33aa324fc27e1
Thales Nascimento <thales.silva.nascimento@gmail.com>
2024-04-01 10:31:32 -0300

    feat: add e2e jest config and env file

---
+++ test/jest-e2e.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  rootDir: '.',
+  testEnvironment: 'node',
+  coverageProvider: 'v8',
+  testRegex: '.e2e-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': '@swc/jest',
+  },
+  setupFilesAfterEnv: ['./jest-setup.ts'],
+};
+
+export default config;
