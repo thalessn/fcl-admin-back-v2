2fe3aeb0e6f2d12baa3759b502a65e5376e03820
Thales Nascimento <thales.silva.nascimento@gmail.com>
2024-04-24 14:49:07 -0300

    feat: add value object to audio and video

--- src/core/video/domain/video.aggregate.ts
+++ src/core/video/domain/video.aggregate.ts
@@ -6,6 +6,8 @@ import { CastMemberId } from '../../cast-member/domain/cast-member.aggregate';
 import { Rating } from './rating.vo';
 import { Banner } from './banner.vo';
 import { Thumbnail } from './thumbnail.vo';
+import { Trailer } from './trailer.vo';
+import { VideoMedia } from './video-media.vo';
 
 export type VideoConstructorProps = {
   video_id?: VideoId;
@@ -16,17 +18,17 @@ export type VideoConstructorProps = {
   rating: Rating;
   is_opened: boolean;
   is_published: boolean;
-
   banner?: Banner;
   thumbnail?: Thumbnail;
   thumbnail_half?: Thumbnail;
+  trailer?: Trailer;
+  video?: VideoMedia;
 
   categories_id: Map<string, CategoryId>;
   genres_id: Map<string, GenreId>;
   cast_members_id: Map<string, CastMemberId>;
   created_at?: Date;
 };
-
 export type VideoCreateCommand = {
   title: string;
   description: string;
@@ -34,18 +36,17 @@ export type VideoCreateCommand = {
   duration: number;
   rating: Rating;
   is_opened: boolean;
-
   banner?: Banner;
   thumbnail?: Thumbnail;
   thumbnail_half?: Thumbnail;
+  trailer?: Trailer;
+  video?: VideoMedia;
 
   categories_id: CategoryId[];
   genres_id: GenreId[];
   cast_members_id: CastMemberId[];
 };
-
 export class VideoId extends Uuid {}
-
 export class Video extends AggregateRoot {
   video_id: VideoId;
   title: string;
@@ -55,16 +56,16 @@ export class Video extends AggregateRoot {
   rating: Rating;
   is_opened: boolean;
   is_published: boolean; //uploads
-
   banner: Banner | null;
   thumbnail: Thumbnail | null;
   thumbnail_half: Thumbnail | null;
+  trailer: Trailer | null;
+  video: VideoMedia | null;
 
   categories_id: Map<string, CategoryId>;
   genres_id: Map<string, GenreId>;
   cast_members_id: Map<string, CastMemberId>;
   created_at: Date;
-
   constructor(props: VideoConstructorProps) {
     super();
     this.video_id = props.video_id ?? new VideoId();
@@ -75,17 +76,17 @@ export class Video extends AggregateRoot {
     this.rating = props.rating;
     this.is_opened = props.is_opened;
     this.is_published = props.is_published;
-
     this.banner = props.banner ?? null;
     this.thumbnail = props.thumbnail ?? null;
     this.thumbnail_half = props.thumbnail_half ?? null;
+    this.trailer = props.trailer ?? null;
+    this.video = props.video ?? null;
 
     this.categories_id = props.categories_id;
     this.genres_id = props.genres_id;
     this.cast_members_id = props.cast_members_id;
     this.created_at = props.created_at ?? new Date();
   }
-
   static create(props: VideoCreateCommand) {
     const video = new Video({
       ...props,
@@ -97,74 +98,58 @@ export class Video extends AggregateRoot {
     video.validate;
     return video;
   }
-
   changeTitle(title: string): void {
     this.title = title;
     this.validate;
   }
-
   changeDescription(description: string): void {
     this.description = description;
   }
-
   changeYearLaunched(yearLaunched: number): void {
     this.year_launched = yearLaunched;
   }
-
   changeDuration(duration: number): void {
     this.duration = duration;
   }
-
   changeRating(rating: Rating): void {
     this.rating = rating;
   }
-
   markAsOpened(): void {
     this.is_opened = true;
   }
-
   markAsNotOpened(): void {
     this.is_opened = false;
   }
-
   addCategoryId(categoryId: CategoryId): void {
     this.categories_id.set(categoryId.id, categoryId);
   }
-
   removeCategoryId(categoryId: CategoryId): void {
     this.categories_id.delete(categoryId.id);
   }
-
   syncCategoriesId(categoriesId: CategoryId[]): void {
     if (!categoriesId.length) {
       throw new Error('Categories id is empty');
     }
     this.categories_id = new Map(categoriesId.map((id) => [id.id, id]));
   }
-
   addGenreId(genreId: GenreId): void {
     this.genres_id.set(genreId.id, genreId);
   }
-
   removeGenreId(genreId: GenreId): void {
     this.genres_id.delete(genreId.id);
   }
-
   syncGenresId(genresId: GenreId[]): void {
     if (!genresId.length) {
       throw new Error('Genres id is empty');
     }
     this.genres_id = new Map(genresId.map((id) => [id.id, id]));
   }
-
   addCastMemberId(castMemberId: CastMemberId): void {
     this.cast_members_id.set(castMemberId.id, castMemberId);
   }
-
   removeCastMemberId(castMemberId: CastMemberId): void {
     this.cast_members_id.delete(castMemberId.id);
   }
-
   syncCastMembersId(castMembersId: CastMemberId[]): void {
     if (!castMembersId.length) {
       throw new Error('Cast Members id is empty');
@@ -181,7 +166,6 @@ export class Video extends AggregateRoot {
   get entity_id() {
     return this.video_id;
   }
-
   toJSON() {
     return {
       video_id: this.video_id.id,
@@ -195,6 +179,8 @@ export class Video extends AggregateRoot {
       banner: this.banner ? this.banner.toJSON() : null,
       thumbnail: this.thumbnail ? this.thumbnail.toJSON() : null,
       thumbnail_half: this.thumbnail_half ? this.thumbnail_half.toJSON() : null,
+      trailer: this.trailer ? this.trailer.toJSON() : null,
+      video: this.video ? this.video.toJSON() : null,
       categories_id: Array.from(this.categories_id.values()).map((id) => id.id),
       genres_id: Array.from(this.genres_id.values()).map((id) => id.id),
       cast_members_id: Array.from(this.cast_members_id.values()).map(
